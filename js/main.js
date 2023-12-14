f8b512242c82e192f6d38a2523c5d032ce46fd19
wizardy <wizardhassan99@gmail.com>
2023-12-14 16:54:49 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 const promise = new Promise((resolve, reject) => {
-  resolve('Hello World !');
-  // reject('Hello World !');
+  // resolve('Hello World !');
+  reject('There is an error !');
 })
 
-promise.then((p) => {
-  console.log(p);
+
+// !- then Method
+// promise.then((p) => {
+//   console.log('Message : ', p);
+// })
+
+
+// !- catch Method
+promise.catch((e) => {
+  console.log('error : ', e)
 })
