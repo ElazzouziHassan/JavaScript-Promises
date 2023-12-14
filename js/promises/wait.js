1def0c05e275d02f1003095106b1577b2ac42e74
wizardy <wizardhassan99@gmail.com>
2023-12-14 18:10:38 +0100

    commit

--- js/promises/wait.js
+++ js/promises/wait.js
@@ -15,15 +15,28 @@ const waitAndFailed = (timer) => {
 };
 
 // test :
-wait(3000).then( p => {
-  console.log('Wait for 3s !');
-  return wait(2000);
-}).then( p => {
-  console.log('Wiat for 2s !');
-  return wait(1000);
-}).then( p => {
-  console.log('Done With Success.')
-  return waitAndFailed(1000)
-}).then(console.log);
+// wait(3000).then( p => {
+//   console.log('Wait for 3s !');
+//   return wait(2000);
+// }).then( p => {
+//   console.log('Wiat for 2s !');
+//   return wait(1000);
+// }).then( p => {
+//   console.log('Done With Success.')
+//   return waitAndFailed(1000)
+// }).then(console.log);
 
 // waitAndFailed(5000).then(console.log).catch(err => console.log(err));
+
+// Async & Await :
+
+const main = async () => {
+  await wait(2000)
+  .then( p => {
+    console.log(p, ' after 2s.')
+    return waitAndFailed(2000)
+  }).catch(err => {
+    console.log(err, ' after 4s')
+  })
+}
+main();
